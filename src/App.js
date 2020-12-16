30dbead80e7f65dd889d7dce7efa3961a4be4c81
zakmos <z.zza@hotmail.com>
2020-12-16 10:23:32 +0100

    added temp details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Handy Home</h1>
+        <h2>Marburg</h2>
+        <p>Am Grün 32,</p>
+        <p>35037 Marburg</p>
+        <p> Deutschland</p>
       </header>
     </div>
   );
