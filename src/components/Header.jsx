bb84f17b59f521c15780a860745ca76a6e4e3023
zakmos <z.zza@hotmail.com>
2020-12-17 00:36:19 +0100

    added tempHeader

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <h3>Handy Home - Marburg</h3>
+        )
+    }
+}
+
+
