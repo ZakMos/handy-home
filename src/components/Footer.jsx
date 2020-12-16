595ead10a3e193c9b842babdb67bf109605f8b4a
zakmos <z.zza@hotmail.com>
2020-12-17 00:35:58 +0100

    added tempFooter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <h2 className="footer-text">Copyright &copy; 2020 Handy Home </h2>
+            </div>
+        )
+    }
+}
+
+export default Footer
